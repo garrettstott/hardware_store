c2928eee5bc17a672ae115735f78d5da04a84920
Garrett Stot <garrettstott@gmail.com>
2016-04-09 10:23:50 -0600

    added some components

---
+++ app/assets/javascripts/components/Home.js.jsx
@@ -0,0 +1,14 @@
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>Hardware Inc</h1>
+        <Inventory />
+      </div>
+    );
+  }
+}
---
+++ app/assets/javascripts/components/Inventory.js.jsx
@@ -0,0 +1,13 @@
+class Inventory extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>Inventory</h1>
+      </div>
+    );
+  }
+}
