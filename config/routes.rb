f4e419bb826abcaaaeaf5bdf15d373745c8bffe9
Garrett Stot <garrettstott@gmail.com>
2016-04-09 09:54:02 -0600

    fixed typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-    devise_for :users, controllers: {
-        sessions: 'users/sessions'
-      }
-    end
     
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
   root 'home#index'
 end
